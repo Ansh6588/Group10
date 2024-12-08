17aa1df5570ae117cb3d3b400200af509f0af71d
Ansh Patel <anshpatel6588@gmail.com>
2024-12-08 18:16:51 -0500

    Added comment and data persistant

--- expense/lib/providers/expense_provider.dart
+++ expense/lib/providers/expense_provider.dart
@@ -1,14 +1,21 @@
 //Author:Ansh Patel
+//Description: This class create a list for expenses where we can add new expenses and notifies listners about changes.
 import 'package:flutter/material.dart';
 import '../models/expense.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 class ExpensesProvider with ChangeNotifier {
-  final List<Expense> _expenses = [];
+  final List<Expense> _expenses = []; //list of expenses
 
-  List<Expense> get expenses => _expenses;
-
-  void addExpense(Expense expense) {
+  List<Expense> get expenses => _expenses; // getter for read only access
+  //method to add new expenses
+  void addExpense(Expense expense) async {
     _expenses.add(expense);
+    
     notifyListeners();
+    // Save the updated list to SharedPreferences
+    final prefs = await SharedPreferences.getInstance();
+    final expenseList = _expenses.map((e) => jsonEncode(e.toJson())).toList();
+    await prefs.setStringList('expenses', expenseList);
   }
 }
